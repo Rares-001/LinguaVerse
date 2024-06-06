820b9651db52b04495ba918c93d65c7e5a029884
Polina <91316935+Polinalavender@users.noreply.github.com>
2024-06-06 06:50:55 -0700

    Add registration and login page

---
+++ LinguaVerse/LogIn.xaml.cs
@@ -0,0 +1,9 @@
+namespace LinguaVerse;
+
+public partial class LogIn : ContentPage
+{
+	public LogIn()
+	{
+		InitializeComponent();
+	}
+}
---
+++ LinguaVerse/Registration.xaml.cs
@@ -0,0 +1,9 @@
+namespace LinguaVerse;
+
+public partial class Registration : ContentPage
+{
+	public Registration()
+	{
+		InitializeComponent();
+	}
+}
