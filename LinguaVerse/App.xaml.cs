d750f10a148c5fe50d8831ade9cd304b23885624
Rares-001 <91318114+Rares-001@users.noreply.github.com>
2024-06-07 14:15:58 +0200

    Added UI Logis still missing

--- LinguaVerse/App.xaml.cs
+++ LinguaVerse/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LinguaVerse
+﻿using LinguaVerse.Views;
+
+namespace LinguaVerse
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new DashboardPage();
         }
     }
 }
---
+++ LinguaVerse/Views/DashboardPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace LinguaVerse.Views
+{
+    public partial class DashboardPage : ContentPage
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
