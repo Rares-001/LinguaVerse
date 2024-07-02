e0e649ebc415f2a673939b3cde2c8d6633586b56
Polina <91316935+Polinalavender@users.noreply.github.com>
2024-07-02 09:18:21 -0700

    Add backend support page

--- LinguaVerse/ViewModel/SupportPage.xaml.cs
+++ LinguaVerse/ViewModel/SupportPage.xaml.cs
@@ -1,9 +1,72 @@
 namespace LinguaVerse;
 
-public partial class SupportPage : ContentPage
+    public partial class SupportPage : ContentPage
+    {
+        public SupportPage()
+        {
+            InitializeComponent();
+            BindingContext = new SupportViewModel();
+        }
+    }
+
+public class SupportViewModel : BindableObject
 {
-	public SupportPage()
-	{
-		InitializeComponent();
-	}
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _email;
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            _email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _subject;
+    public string Subject
+    {
+        get => _subject;
+        set
+        {
+            _subject = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _message;
+    public string Message
+    {
+        get => _message;
+        set
+        {
+            _message = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _responseStatus;
+    public string ResponseStatus
+    {
+        get => _responseStatus;
+        set
+        {
+            _responseStatus = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Command SendMessageCommand => new Command(async () => await SendMessage());
+
+
 }
